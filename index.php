3d903e63aebc1b877c3475d10c0654cb9acbcabf
likebeta <ixxoo.me@gmail.com>
2018-01-06 13:08:45 +0800

    update

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>App Center</title>
-	<script src="http://cdn.staticfile.org/jquery/1.9.1/jquery.min.js"></script>
-	<script src="http://cdn.staticfile.org/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-	<script src="http://cdn.staticfile.org/html5shiv/3.6.2/html5shiv.min.js"></script>
-	<link href="http://cdn.staticfile.org/twitter-bootstrap/2.3.1/css/bootstrap.min.css" rel="stylesheet" media="screen" />
-	<link href="http://cdn.staticfile.org/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen" />
+	<script src="//cdn.bootcss.com/jquery/1.9.1/jquery.min.js"></script>
+	<script src="//cdn.bootcss.com/bootstrap/2.3.1/js/bootstrap.min.js"></script>
+	<script src="//cdn.bootcss.com/html5shiv/3.6.2/html5shiv.min.js"></script>
+	<link href="//cdn.bootcss.com/bootstrap/2.3.1/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+	<link href="//cdn.bootcss.com/bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen" />
 	<link href="media/css/base.css" rel="stylesheet" media="screen" />
 </head>
 <body>
